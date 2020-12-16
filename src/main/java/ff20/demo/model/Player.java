68d6858cda5acfa166b7a3bcaa9b85337984c7e4
NickWanchai <wanchai93@hotmail.dk>
2020-12-16 16:14:43 +0100

    Initial commit

---
+++ src/main/java/ff20/demo/model/Player.java
@@ -0,0 +1,70 @@
+package ff20.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private int age;
+    private String name;
+    private String position;
+    private String nationality;
+
+
+    public Player() {
+    }
+
+    public Player(long id, int age, String name, String position, String nationality) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.position = position;
+        this.nationality = nationality;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+}
